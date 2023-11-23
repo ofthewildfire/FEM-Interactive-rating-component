2be6d83b68488e1ae55ddbfe557b482259bcbcf3
ofthewildfire <lissajace@gmail.com>
2023-11-23 17:42:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Interactive rating component
+
+This is a Frontendmentor.io challenge | the
+[interactive rating component](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI)
+which I created using Vanilla JavaScript, CSS and HTML.
+
+I had a lot of fun, and a little bit of learning with the JS logic in regards to
+having only the `checked` item within the radio button group be highlighted with
+the different colour. This actually caused a bit of thinking and I loved that.
+
+I also made some additional "good to have" changes - a button that returns you
+to the main rating selection after you have already made your selection.
+
+## Changes I Want to Make
+
+Currently it is not in any way shape or form pixel perfect, I wanted to work on
+the logic, but I also want to make it look a lot better than it currently is.
+
+- Update design so it aligns more to the provided `jpeg`
+- Read more information on how to work with heights/widths in CSS, it still
+  confused me.
+
+## Contributing
+
+Welcome to correct anything you see :) or have advice on.
---
+++ js/main.js
@@ -0,0 +1,44 @@
+const selectionBtns = document.querySelectorAll('input[type="radio"]')
+const buttonsList = document.querySelectorAll('input[type="radio"]')
+const displayRating = document.querySelector(".rating")
+const submitBtn = document.querySelector(".submit-btn")
+const selectCard = document.querySelector(".card-selection")
+const tyCard = document.querySelector(".card-thankyou")
+const returnToRatingCard = document.querySelector(".return-btn")
+const error = document.querySelector(".error")
+let rating
+
+Array.from(selectionBtns, (btn) => {
+	btn.addEventListener("click", (e) => {
+		rating = e.target.value
+	})
+})
+
+submitBtn.addEventListener("click", onSubmitChange)
+
+Array.from(buttonsList, (btn) => {
+	btn.addEventListener("change", (e) => {
+		const previous = document.querySelector(".active")
+		if (previous) {
+			previous.classList.remove("active")
+		}
+		btn.parentElement.classList.add("active")
+	})
+})
+
+function onSubmitChange() {
+	if (rating) {
+		error.style.display = "none"
+		selectCard.classList.add("hidden")
+		tyCard.classList.remove("hidden")
+		displayRating.innerText = rating
+	} else {
+		error.style.display = "block"
+	}
+}
+
+returnToRatingCard.addEventListener("click", () => {
+	selectCard.classList.remove("hidden")
+	tyCard.classList.add("hidden")
+	rating = ""
+})
